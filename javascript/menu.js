eef53f6f2d90d8f5633239ceae6c19c3713a0a00
NickollasMoraes <nickollas.pa@gmail.com>
2022-03-21 11:54:36 -0300

    att

--- javascript/menu.js
+++ javascript/menu.js
@@ -15,3 +15,16 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+document.querySelector('#noticia1-js').addEventListener('click', () => {
+  window.location = 'https://www.youtube.com/watch?v=MdA3C6Yj7Eo&t=5s'
+})
+document.querySelector('#noticia2-js').addEventListener('click', () => {
+  window.location = 'https://youtu.be/yiIFLjp14Bo'
+})
+document.querySelector('#noticia3-js').addEventListener('click', () => {
+  window.location = 'https://youtu.be/UTSEZlojEAc'
+})
+document.querySelector('#noticia4-js').addEventListener('click', () => {
+  window.location = 'https://youtu.be/K9mFqZUyrM8'
+})
